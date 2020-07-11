418b907fce923818ce3d22ae1ea79b8d1cba25c2
Frank Xu <frankxu2004@gmail.com>
2020-07-11 08:07:44 -0400

    update firefox init script

--- initialize_firefox.sh
+++ initialize_firefox.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# remove existing profiles
 rm -rf /home/vagrant/.mozilla/firefox/dqy0r712.default-release/
+rm -rf /home/vagrant/.mozilla/firefox/m9ztmqyf.default/
+
 unzip -o -q /vagrant/firefox-profile.zip -d /home/vagrant/.mozilla/firefox/
